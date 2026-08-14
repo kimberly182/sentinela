bd9b8e03fdd47a4bab8cf1469f5e4fe1a2f15d67
Kimberly <kimberlyhiasmin4@gmail.com>
2026-08-14 08:28:59 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -187,6 +187,8 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-app.listen(3000, () => {
-  console.log("🏥 Hospital Pro rodando em http://localhost:3000");
+const PORT = process.env.POST
+|| 3000;
+app.listen(PORT, () => {
+  console.log(`Porta ${PORT}`);
 });
