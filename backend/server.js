6cc3b45650c57a931f4e72e4b390139a0fce4b92
Kimberly <kimberlyhiasmin4@gmail.com>
2026-08-18 09:27:06 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,7 @@ function readDB() {
       tv_chamada: null,
       tv_historico: []
     };
-  };
-}
+  }
   const db = JSON.parse(fs.readFileSync(DB_FILE));
   if (!db.tv_chamada) db.tv_chamada = null;
   if (!db.tv_historico) db.tv_historico = [];
