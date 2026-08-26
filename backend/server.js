c497940b1b88d05c1c9b2eb8321b48a28537e487
Kimberly <kimberlyhiasmin4@gmail.com>
2026-08-26 09:44:16 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,6 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const cors = require("cors");
-
-const app = express();
-
-app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "../frontend")));
-
 const DB_FILE = path.join(__dirname, "db.json");
-
 function readDB() {
   if (!fs.existsSync(DB_FILE)) {
     return {
@@ -22,29 +11,8 @@ function readDB() {
       tv_chamada: null,
       tv_historico: []
     };
-  }
-  const db = JSON.parse(fs.readFileSync(DB_FILE));
+    const db = JSON.parse(fs.readFileSync(DB_FILE));
   if (!db.tv_chamada) db.tv_chamada = null;
   if (!db.tv_historico) db.tv_historico = [];
   return db;
 }
-
-function writeDB(data) {
-  fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
-}
-
-// LOGIN
-app.post("/login", (req, res) => {
-  const db = readDB();
-
-  const user = db.usuarios.find(u =>
-    u.usuario === req.body.usuario &&
-    u.senha === req.body.senha
-  );
-
-  if (!user) {
-    return res.status(401).json({ erro: "Login inválido" });
-  }
-
-  res.json(user);
-});
